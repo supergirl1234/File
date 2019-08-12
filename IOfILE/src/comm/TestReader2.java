ed0021d74577bb332fd5a1ee411636652462efbf
范蕾蕾 <2547462712@qq.com>
2019-08-12 15:29:03 +0800

    无

--- IOfILE/src/comm/TestReader2.java
+++ IOfILE/src/comm/TestReader2.java
@@ -8,14 +8,11 @@ public class TestReader2 {
     public static void main(String[] args) {
 
         File infile = new File("D:/test/input.txt");
-
         Reader reader=null;
         try {
             reader=new FileReader(infile);
-
             int len=-1;
             char[] chars=new char[3];
-
             while((len=reader.read(chars))!=-1){
                 System.out.println(chars);
             }
